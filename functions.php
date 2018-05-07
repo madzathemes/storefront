cf5831fbed7807d6231f35c47b1a175faa6fe51a
madzathemes <madza@madzathemes.com>
2018-05-07 11:33:33 +0300

    fix

--- functions.php
+++ functions.php
@@ -49,6 +49,88 @@ if ( is_admin() ) {
 	require 'inc/admin/class-storefront-plugin-install.php';
 }
 
+add_action( 'woocommerce_before_single_product', 'move_variations_single_price', 1 );
+function move_variations_single_price(){
+    global $product, $post;
+
+    if ( $product->is_type( 'variable' ) ) {
+        // removing the variations price for variable products
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+
+        // Change location and inserting back the variations price
+        add_action( 'woocommerce_single_product_summary', 'replace_variation_single_price', 10 );
+    }
+}
+
+add_filter( 'woocommerce_variable_sale_price_html', 'bbloomer_variation_price_format', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'bbloomer_variation_price_format', 10, 2 );
+
+function bbloomer_variation_price_format( $price, $product ) {
+
+// Main Price
+$prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+$price = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+// Sale Price
+$prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+sort( $prices );
+$saleprice = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+if ( $price !== $saleprice ) {
+$price = '<del>' . $saleprice . $product->get_price_suffix() . '</del> <ins>' . $price . $product->get_price_suffix() . '</ins>';
+}
+return $price;
+}
+function replace_variation_single_price(){
+    global $product;
+
+    // Main Price
+    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+    $price = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+    // Sale Price
+    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+    sort( $prices );
+    $saleprice = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+    if ( $price !== $saleprice && $product->is_on_sale() ) {
+        $price = '<del>' . $saleprice . $product->get_price_suffix() . '</del> <ins>' . $price . $product->get_price_suffix() . '</ins>';
+    }
+
+    ?>
+    <style>
+        div.woocommerce-variation-price,
+        div.woocommerce-variation-availability,
+        div.hidden-variable-price {
+            height: 0px !important;
+            overflow:hidden;
+            position:relative;
+            line-height: 0px !important;
+            font-size: 0% !important;
+        }
+    </style>
+    <script>
+    jQuery(document).ready(function($) {
+        $('select').blur( function(){
+            if( '' != $('input.variation_id').val() ){
+                if($('p.availability'))
+                    $('p.availability').remove();
+                $('p.price').html($('div.woocommerce-variation-price > span.price').html()).append('<p class="availability">'+$('div.woocommerce-variation-availability').html()+'</p>');
+                console.log($('input.variation_id').val());
+            } else {
+                $('p.price').html($('div.hidden-variable-price').html());
+                if($('p.availability'))
+                    $('p.availability').remove();
+                console.log('NULL');
+            }
+        });
+    });
+    </script>
+    <?php
+
+    echo '<p class="price">'.$price.'</p>
+    <div class="hidden-variable-price" >'.$price.'</div>';
+}
 /**
  * NUX
  * Only load if wp version is 4.7.3 or above because of this issue;
