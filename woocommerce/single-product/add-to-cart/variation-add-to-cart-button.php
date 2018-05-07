b47bbbc952c42a781a7d1489fbc48226894881f9
madzathemes <madza@madzathemes.com>
2018-05-07 20:51:40 +0300

    fix

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -13,8 +13,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 ?>
-<div class="woocommerce-variation-add-to-cart variations_button">
-	<span class="single-quantity">Quantity</span>
+<div class="single-quantity-wrap">
+	<div class="single-quantity">Quantity</div>
 	<?php
 		/**
 		 * @since 3.0.0.
@@ -32,6 +32,9 @@ global $product;
 		 */
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
+</div>
+<div class="woocommerce-variation-add-to-cart variations_button">
+
 	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
