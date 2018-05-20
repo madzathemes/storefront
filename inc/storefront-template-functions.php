8a04c7cf9608629b6db84b8d8f04184051904287
madzathemes <madza@madzathemes.com>
2018-05-20 12:59:29 +0300

    fix

--- inc/storefront-template-functions.php
+++ inc/storefront-template-functions.php
@@ -1008,7 +1008,7 @@ function rc_woocommerce_recently_viewed_products( $atts, $content = null ) {
    <!-- //put your theme html loop hare -->
  <li class="product liamrecent">
     <a class="product-picture" href="<?php echo get_post_permalink(); ?>" title="Show details for Watches">
-        <img alt="Picture of Watches" src="<?php echo $url;?>" title="Show details for Watches" />
+        <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="lazyload"  data-src="<?php echo $url; ?>"/>
 				<h2><?php the_title()?></h2>
 				<span class="woocommerce-Price-amount amount"><?php global $product; echo $product->get_price_html(); ?></span>
     </a>
